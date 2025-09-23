859c06c91b9934c645a76b3a01f1bfc5a58f9ed6
gabriela <gabriela.rodrigues.1@academico.ifpb.edu.br>
2025-09-23 11:33:58 -0300

    Tela de cadastro

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,57 +2,27 @@ import { Image } from 'expo-image';
 import { StyleSheet } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: '#ffffffff', dark: '#f0ededff' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/icon-cadastro.png')}
           style={styles.reactLogo}
         />
       }>
-      
-      {/* Título da Página */}
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Produtos d'Paris</ThemedText>
-      </ThemedView>
-
-      {/* Área de conteúdo (vazia para você preencher depois) */}
-      <ThemedView style={styles.card}>
-        <ThemedText type="default">
-          Bem-vindo ao app! Aqui você pode adicionar o conteúdo inicial da sua página.
-        </ThemedText>
-      </ThemedView>
-
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 16,
-  },
-  card: {
-    backgroundColor: '#fff',
-    padding: 16,
-    borderRadius: 12,
-    marginBottom: 16,
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowRadius: 6,
-    elevation: 3, // sombra no Android
-  },
-  reactLogo: {
-    height: 178,
+  reactLogo: { 
+    height: 180,
     width: 290,
     bottom: 0,
-    left: 0,
+    left: 45,
     position: 'absolute',
   },
 });
